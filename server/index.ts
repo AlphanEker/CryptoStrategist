101423142e8b33de9b47cc1b110af1bd5b2b82ab
Semih Kaan Cakir <kaan.cakir@ug.bilkent.edu.tr>
2023-11-20 17:47:05 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,66 @@
 import express, { Express, Request, Response } from 'express';
 
-
 const app: Express = express();
 const port = 8180;
 
+// Middleware to parse JSON in request body
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('Express + TypeScript Serve');
+});
+
+// Get Current Exchange Rate
+app.get('/GetRate', (req: Request, res: Response) => {
+  res.send('500');
+});
+
+// Update Exchange Rate
+app.get('/UpdateRate', (req: Request, res: Response) => {
+  res.send('600');
+});
+
+// Update Balance
+app.post('/UpdateBalance', (req: Request, res: Response) => {
+  // Assuming you send JSON with { "balance": newBalance } in the request body
+  const newBalance: number = req.body.balance;
+  // Logic to update the balance in your system
+  // ...
+
+  res.send(`Balance updated to ${newBalance}`);
+});
+
+// Buy
+app.post('/Buy', (req: Request, res: Response) => {
+  // Assuming you send JSON with { "amount": buyAmount, "price": buyPrice } in the request body
+  const buyAmount: number = req.body.amount;
+  const buyPrice: number = req.body.price;
+  // Logic to execute buy order
+  // ...
+
+  res.send(`Bought ${buyAmount} at ${buyPrice}`);
+});
+
+// Sell
+app.post('/Sell', (req: Request, res: Response) => {
+  // Assuming you send JSON with { "amount": sellAmount, "price": sellPrice } in the request body
+  const sellAmount: number = req.body.amount;
+  const sellPrice: number = req.body.price;
+  // Logic to execute sell order
+  // ...
+
+  res.send(`Sold ${sellAmount} at ${sellPrice}`);
+});
+
+// Update Coin Balance
+app.post('/UpdateCoinBalance', (req: Request, res: Response) => {
+  // Assuming you send JSON with { "coin": "BTC", "balance": newCoinBalance } in the request body
+  const coin: string = req.body.coin;
+  const newCoinBalance: number = req.body.balance;
+  // Logic to update the coin balance in your system
+  // ...
+
+  res.send(`Updated ${coin} balance to ${newCoinBalance}`);
 });
 
 app.listen(port, () => {
